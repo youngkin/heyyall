8e896b3d5fd308be0473b6c4d2a93c46373b50e6
youngkin <rich.youngkin@gmail.com>
2020-05-09 16:15:18 -0600

    Implemented ResponseHandler tests.
    Implemented TestResponseHandlerHappyPath test. Refactored ResponseHandler
    a bit more to make testing easier, pulled
    out common 'RqstStats' which also made it possible to compare those
    structs directly. So now it's relatively easy to do mostly struct
    compares. Only timing dependent fields aren't verified.

--- internal/responseHandler.go
+++ internal/responseHandler.go
@@ -6,7 +6,6 @@ package internal
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"math"
 	"time"
@@ -14,30 +13,41 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+// RqstStats contains a set of common runtime stats reported at both the
+// Summary and Endpoint level
+type RqstStats struct {
+	// TotalRqsts is the overall number of requests made during the run
+	TotalRqsts int64
+	// TotalRequestDuration is the sum of all request run durations
+	TotalRequestDuration time.Duration
+	// TotalRequestDurationStr is the string version of TotalRequestDuration
+	TotalRequestDurationStr string
+	// MaxRqstDuration is the longest request duration
+	MaxRqstDuration time.Duration
+	// MaxRqstDurationStr is a string representation of MaxRqstDuration
+	MaxRqstDurationStr string
+	// MinRqstDuration is the smallest request duration for an endpoint
+	MinRqstDuration time.Duration
+	// MinRqstDurationStr is a string representation of MinRqstDuration
+	MinRqstDurationStr string
+	// AvgRqstDuration is the average duration of a request for an endpoint
+	AvgRqstDuration time.Duration
+	// AvgRqstDurationStr is the average duration of a request in microseconds
+	AvgRqstDurationStr string
+}
+
 // EndpointSummary is used to report an overview of the results of
 // a load test run for a given endpoint.
 type EndpointSummary struct {
-	URL string
-	// Method is the HTTP Method (e.g., GET, PUT, POST, DELETE)
-	Method string
-	// totalRequestDuration is the sum of all request run durations in seconds
-	totalRequestDuration time.Duration
 	// URL is the endpoint URL
-	// HTTPStatusDist is a map of HTTP Status (e.g., 200, 201, 404, etc)
-	// to the number of occurrences (value) for a given status (key)
-	HTTPStatusDist map[int]int
-	// TotalRqsts is the overall number of requests made during the run
-	TotalRqsts int64
-	// TotalRequestDuration is the sum of all request run durations in seconds
-	TotalRequestDuration string
-	// MaxRqstDuration is the longest request duration in microseconds
-	maxRqstDuration time.Duration
-	MaxRqstDuration string
-	// MinRqstDuration is the smallest request duration in microseconds
-	minRqstDuration time.Duration
-	MinRqstDuration string
-	// AvgRqstDuration is the average duration of a request in microseconds
-	AvgRqstDuration string
+	URL string
+	// HTTPMethodStatusDist summarizes, by HTTP method, the number of times a
+	// given status was returned (e.g., 200, 201, 404, etc). More specifically,
+	// it is a map keyed by HTTP method containing a map keyed by HTTP status
+	// referencing the number of times that status was returned.
+	HTTPMethodStatusDist map[string]map[int]int
+	// RqstStats is a summary of runtime statistics
+	RqstStats RqstStats
 }
 
 // RunSummary is used to report an overview of the results of a
@@ -46,8 +56,10 @@ type RunSummary struct {
 	// RqstRatePerSec is the overall request rate per second
 	// rounded to the nearest integer
 	RqstRatePerSec float64
-	// RunDuration is the wall clock time of the test in seconds
-	RunDuration string
+	// RunDuration is the wall clock duration of the test
+	RunDuration time.Duration
+	// RunDurationStr is the string representation of RunDuration
+	RunDurationStr string
 	// ResponseDistribution is distribution of response times. There will be
 	// 11 bucket; 10 microseconds or less, between 10us and 100us,
 	// 100us and 1ms, 1ms to 10ms, 10ms to 100ms, 100ms to 1s, 1s to 1.1s,
@@ -61,18 +73,8 @@ type RunSummary struct {
 	// MinRqstRatePerSec is the maximum request rate per second
 	// over 1/10th of the run duration or number of requests
 	//MinRqstRatePerSec int
-	// TotalRqsts is the overall number of requests made during the run
-	TotalRqsts int64
-	// TotalRequestDuration is the sum of all request run durations in seconds
-	TotalRequestDuration string
-	// MaxRqstDuration is the longest request duration in microseconds
-	maxRqstDuration time.Duration
-	MaxRqstDuration string
-	// MinRqstDuration is the smallest request duration in microseconds
-	minRqstDuration time.Duration
-	MinRqstDuration string
-	// AvgRqstDuration is the average duration of a request in microseconds
-	AvgRqstDuration string
+	// RqstStats is a summary of runtime statistics
+	RqstStats RqstStats
 	// EndpointOverviewSummary describes how often each endpoint was called.
 	// It is a map keyed by URL of a map keyed by HTTP verb with a value of
 	// number of requests. So it's a summary of how often each HTTP verb
@@ -93,7 +95,7 @@ type ResponseHandler struct {
 func (rh ResponseHandler) Start() {
 	log.Debug().Msg("ResponseHandler starting")
 	epRunSummary := make(map[string]*EndpointSummary)
-	runSummary := RunSummary{maxRqstDuration: -1, minRqstDuration: time.Duration(math.MaxInt64)}
+	runSummary := RunSummary{RqstStats: RqstStats{MaxRqstDuration: time.Duration(-1), MinRqstDuration: time.Duration(math.MaxInt64)}}
 	runSummary.EndpointOverviewSummary = make(map[string]map[string]int)
 
 	start := time.Now()
@@ -128,36 +130,32 @@ func (rh ResponseHandler) Start() {
 	}
 }
 
-func (rh ResponseHandler) finalizeResponseStats(start time.Time, totalRunTime *time.Duration, runSummary *RunSummary, epRunSummary map[string]*EndpointSummary) error {
-	runTime := time.Since(start)
-	runSummary.RunDuration = runTime.String()
-	runSummary.TotalRequestDuration = totalRunTime.String()
-	runSummary.MaxRqstDuration = runSummary.maxRqstDuration.String()
-	runSummary.MinRqstDuration = runSummary.minRqstDuration.String()
-	avgRqstDuration := time.Duration(0)
-	if runSummary.TotalRqsts > 0 {
-		avgRqstDuration = *totalRunTime / time.Duration(runSummary.TotalRqsts)
-	}
-	runSummary.AvgRqstDuration = avgRqstDuration.String()
+func (rh ResponseHandler) finalizeResponseStats(start time.Time, totalRunTime *time.Duration,
+	runSummary *RunSummary, epRunSummary map[string]*EndpointSummary) error {
 
-	// run times shorter than 1 millisecond will result in a 'RqstRatePerSec' being zero due to rounding
-	runDurInMillis := runTime / time.Millisecond
-	if runDurInMillis < 1 {
-		return errors.New("Run duration in milliseconds was less than 1")
+	runSummary.RunDuration = time.Since(start)
+	runSummary.RunDurationStr = runSummary.RunDuration.String()
+	runSummary.RqstStats.TotalRequestDurationStr = totalRunTime.String()
+	runSummary.RqstStats.MaxRqstDurationStr = runSummary.RqstStats.MaxRqstDuration.String()
+	runSummary.RqstStats.MinRqstDurationStr = runSummary.RqstStats.MinRqstDuration.String()
+	runSummary.RqstStats.AvgRqstDuration = time.Duration(0)
+	if runSummary.RqstStats.TotalRqsts > 0 {
+		runSummary.RqstStats.AvgRqstDuration = *totalRunTime / time.Duration(runSummary.RqstStats.TotalRqsts)
 	}
-	runSummary.RqstRatePerSec = (float64(runSummary.TotalRqsts) / float64(runTime)) * float64(time.Second)
-	log.Debug().Msgf("NumRqsts: %d, RunDur in millis: %d, Rqsts/millis: %f, TotalRqsts/RunDur: %f", runSummary.TotalRqsts,
-		int64(runDurInMillis), runSummary.RqstRatePerSec, float64(runSummary.TotalRqsts)/float64(runDurInMillis))
+	runSummary.RqstStats.AvgRqstDurationStr = runSummary.RqstStats.AvgRqstDuration.String()
+
+	runSummary.RqstRatePerSec = (float64(runSummary.RqstStats.TotalRqsts) / float64(runSummary.RunDuration)) * float64(time.Second)
 	runSummary.EndpointRunSummary = epRunSummary
 
 	for _, epSumm := range epRunSummary {
-		epSumm.MaxRqstDuration = epSumm.maxRqstDuration.String()
-		epSumm.MinRqstDuration = epSumm.minRqstDuration.String()
-		epSumm.AvgRqstDuration = "0s"
-		if epSumm.TotalRqsts > 0 {
-			epSumm.AvgRqstDuration = (epSumm.totalRequestDuration / time.Duration(epSumm.TotalRqsts)).String()
+		epSumm.RqstStats.MaxRqstDurationStr = epSumm.RqstStats.MaxRqstDuration.String()
+		epSumm.RqstStats.MinRqstDurationStr = epSumm.RqstStats.MinRqstDuration.String()
+		epSumm.RqstStats.AvgRqstDurationStr = "0s"
+		if epSumm.RqstStats.TotalRqsts > 0 {
+			epSumm.RqstStats.AvgRqstDuration = (epSumm.RqstStats.TotalRequestDuration / time.Duration(epSumm.RqstStats.TotalRqsts))
+			epSumm.RqstStats.AvgRqstDurationStr = epSumm.RqstStats.AvgRqstDuration.String()
 		}
-		epSumm.TotalRequestDuration = epSumm.totalRequestDuration.String()
+		epSumm.RqstStats.TotalRequestDurationStr = epSumm.RqstStats.TotalRequestDuration.String()
 		log.Debug().Msgf("EndpointSummary: %+v", epSumm)
 	}
 
@@ -165,49 +163,53 @@ func (rh ResponseHandler) finalizeResponseStats(start time.Time, totalRunTime *t
 }
 
 func accumulateResponseStats(resp Response, totalRunTime *time.Duration, runSummary *RunSummary, epRunSummary map[string]*EndpointSummary) {
-	runSummary.TotalRqsts++
+	runSummary.RqstStats.TotalRqsts++
+	runSummary.RqstStats.TotalRequestDuration += resp.RequestDuration
 	*totalRunTime = *totalRunTime + resp.RequestDuration
-	if resp.RequestDuration > runSummary.maxRqstDuration {
-		runSummary.maxRqstDuration = resp.RequestDuration
+	if resp.RequestDuration > runSummary.RqstStats.MaxRqstDuration {
+		runSummary.RqstStats.MaxRqstDuration = resp.RequestDuration
 	}
-	if resp.RequestDuration < runSummary.minRqstDuration {
-		runSummary.minRqstDuration = resp.RequestDuration
+	if resp.RequestDuration < runSummary.RqstStats.MinRqstDuration {
+		runSummary.RqstStats.MinRqstDuration = resp.RequestDuration
 	}
 
-	var eqRqstCount map[string]int
-	eqRqstCount, found := runSummary.EndpointOverviewSummary[resp.Endpoint.URL]
+	var epStatusCount map[string]int
+	epStatusCount, found := runSummary.EndpointOverviewSummary[resp.Endpoint.URL]
 	if !found {
 		runSummary.EndpointOverviewSummary[resp.Endpoint.URL] = make(map[string]int)
-		eqRqstCount = runSummary.EndpointOverviewSummary[resp.Endpoint.URL]
+		epStatusCount = runSummary.EndpointOverviewSummary[resp.Endpoint.URL]
 	}
-	eqRqstCount[resp.Endpoint.Method]++
+	epStatusCount[resp.Endpoint.Method]++
 
 	var epSumm *EndpointSummary
 	epSumm, found = epRunSummary[resp.Endpoint.URL]
 	if !found {
 		epSumm = &EndpointSummary{
-			URL:             resp.Endpoint.URL,
-			Method:          resp.Endpoint.Method,
-			HTTPStatusDist:  make(map[int]int),
-			maxRqstDuration: -1,
-			minRqstDuration: time.Duration(math.MaxInt64),
+			URL:                  resp.Endpoint.URL,
+			HTTPMethodStatusDist: make(map[string]map[int]int),
+			RqstStats: RqstStats{
+				MaxRqstDuration: -1,
+				MinRqstDuration: time.Duration(math.MaxInt64),
+			},
 		}
 		epRunSummary[resp.Endpoint.URL] = epSumm
 	}
 
-	epSumm.TotalRqsts++
-	epSumm.totalRequestDuration = epSumm.totalRequestDuration + resp.RequestDuration
+	epSumm.RqstStats.TotalRqsts++
+	epSumm.RqstStats.TotalRequestDuration = epSumm.RqstStats.TotalRequestDuration + resp.RequestDuration
 
-	if resp.RequestDuration > epSumm.maxRqstDuration {
-		epSumm.maxRqstDuration = resp.RequestDuration
+	if resp.RequestDuration > epSumm.RqstStats.MaxRqstDuration {
+		epSumm.RqstStats.MaxRqstDuration = resp.RequestDuration
 	}
-	if resp.RequestDuration < epSumm.minRqstDuration {
-		epSumm.minRqstDuration = resp.RequestDuration
+	if resp.RequestDuration < epSumm.RqstStats.MinRqstDuration {
+		epSumm.RqstStats.MinRqstDuration = resp.RequestDuration
 	}
 
-	_, ok := epSumm.HTTPStatusDist[resp.HTTPStatus]
+	_, ok := epSumm.HTTPMethodStatusDist[resp.Endpoint.Method]
 	if !ok {
-		epSumm.HTTPStatusDist[resp.HTTPStatus] = 0
+		epSumm.HTTPMethodStatusDist[resp.Endpoint.Method] = make(map[int]int)
+		epSumm.HTTPMethodStatusDist[resp.Endpoint.Method][resp.HTTPStatus] = 0 // This is correct. It'll be incremented below
 	}
-	epSumm.HTTPStatusDist[resp.HTTPStatus]++
+	epSumm.HTTPMethodStatusDist[resp.Endpoint.Method][resp.HTTPStatus]++
+
 }
--- internal/responseHandler_test.go
+++ internal/responseHandler_test.go
@@ -5,11 +5,17 @@
 package internal
 
 import (
+	"encoding/json"
 	"flag"
 	"io/ioutil"
+	"math"
+	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
+
+	"github.com/youngkin/heyyall/api"
 )
 
 var (
@@ -69,11 +75,176 @@ func TestResponseHandlerHappyPath(t *testing.T) {
 	}
 
 	expected := readGoldenFile(t, testName)
-	if expected != string(actual) {
-		t.Errorf("expected %s, got %s", expected, string(actual))
+	if string(expected) != string(actual) {
+		t.Errorf("expected %s, got %s", string(expected), string(actual))
 	}
 
 	os.Stdout = rescueStdout
+}
+
+func TestResponseStats(t *testing.T) {
+	testName := "TestResponseStats"
+	start := time.Now()
+	time.Sleep(time.Millisecond * 1) // Test needs a runtime of at least 1ms
+	url1 := "http://someurl/1"
+	url2 := "http://someurl/2"
+	url3 := "http://someurl/3"
+	runSummary := RunSummary{
+		RqstStats: RqstStats{
+			MinRqstDuration: math.MaxInt64,
+			MaxRqstDuration: 0,
+		},
+		EndpointOverviewSummary: make(map[string]map[string]int),
+	}
+	epRunSummary := make(map[string]*EndpointSummary)
+
+	// URL1
+	resp := Response{
+		HTTPStatus:      http.StatusNotFound,
+		Endpoint:        api.Endpoint{URL: url1, Method: http.MethodGet},
+		RequestDuration: time.Millisecond * 100,
+	}
+	totalRunTime := time.Second * 0
+	accumulateResponseStats(resp, &totalRunTime, &runSummary, epRunSummary)
+
+	resp = Response{
+		HTTPStatus:      http.StatusAccepted,
+		Endpoint:        api.Endpoint{URL: url1, Method: http.MethodPut},
+		RequestDuration: time.Millisecond * 1000,
+	}
+	accumulateResponseStats(resp, &totalRunTime, &runSummary, epRunSummary)
+	resp = Response{
+		HTTPStatus:      http.StatusAccepted,
+		Endpoint:        api.Endpoint{URL: url1, Method: http.MethodPut},
+		RequestDuration: time.Millisecond * 500,
+	}
+	accumulateResponseStats(resp, &totalRunTime, &runSummary, epRunSummary)
+
+	// URL2
+	resp = Response{
+		HTTPStatus:      http.StatusCreated,
+		Endpoint:        api.Endpoint{URL: url2, Method: http.MethodPost},
+		RequestDuration: time.Millisecond * 250,
+	}
+	accumulateResponseStats(resp, &totalRunTime, &runSummary, epRunSummary)
+
+	// URL3 - POST
+	resp = Response{
+		HTTPStatus:      http.StatusCreated,
+		Endpoint:        api.Endpoint{URL: url3, Method: http.MethodPost},
+		RequestDuration: time.Millisecond * 250,
+	}
+	accumulateResponseStats(resp, &totalRunTime, &runSummary, epRunSummary)
+	// GET
+	resp = Response{
+		HTTPStatus:      http.StatusOK,
+		Endpoint:        api.Endpoint{URL: url3, Method: http.MethodGet},
+		RequestDuration: time.Millisecond * 250,
+	}
+	accumulateResponseStats(resp, &totalRunTime, &runSummary, epRunSummary)
+	resp = Response{
+		HTTPStatus:      http.StatusOK,
+		Endpoint:        api.Endpoint{URL: url3, Method: http.MethodGet},
+		RequestDuration: time.Millisecond * 750,
+	}
+	accumulateResponseStats(resp, &totalRunTime, &runSummary, epRunSummary)
+	// PUT
+	resp = Response{
+		HTTPStatus:      http.StatusOK,
+		Endpoint:        api.Endpoint{URL: url3, Method: http.MethodPut},
+		RequestDuration: time.Millisecond * 250,
+	}
+	accumulateResponseStats(resp, &totalRunTime, &runSummary, epRunSummary)
+	resp = Response{
+		HTTPStatus:      http.StatusOK,
+		Endpoint:        api.Endpoint{URL: url3, Method: http.MethodPut},
+		RequestDuration: time.Millisecond * 750,
+	}
+	accumulateResponseStats(resp, &totalRunTime, &runSummary, epRunSummary)
+	resp = Response{
+		HTTPStatus:      http.StatusOK,
+		Endpoint:        api.Endpoint{URL: url3, Method: http.MethodPut},
+		RequestDuration: time.Millisecond * 1250,
+	}
+	accumulateResponseStats(resp, &totalRunTime, &runSummary, epRunSummary)
+	resp = Response{
+		HTTPStatus:      http.StatusOK,
+		Endpoint:        api.Endpoint{URL: url3, Method: http.MethodPut},
+		RequestDuration: time.Millisecond * 1750,
+	}
+	accumulateResponseStats(resp, &totalRunTime, &runSummary, epRunSummary)
+	// DELETE
+	resp = Response{
+		HTTPStatus:      http.StatusOK,
+		Endpoint:        api.Endpoint{URL: url3, Method: http.MethodDelete},
+		RequestDuration: time.Millisecond * 900,
+	}
+	accumulateResponseStats(resp, &totalRunTime, &runSummary, epRunSummary)
+
+	// FINALIZE
+	rh := ResponseHandler{}
+	err := rh.finalizeResponseStats(start, &totalRunTime, &runSummary, epRunSummary)
+	if err != nil {
+		t.Errorf("unexpected error finalizing response stats: %s", err)
+	}
+	actual, err := json.Marshal(runSummary)
+	if err != nil {
+		t.Errorf("error marshaling RunSummary into string: %+v. Error: %s\n", runSummary, err)
+	}
+
+	// fmt.Printf("\n%s\n\n", string(actual))
+
+	if *update {
+		updateGoldenFile(t, testName, string(actual))
+	}
+
+	expected := readGoldenFile(t, testName)
+	expectedJSON := RunSummary{}
+	err = json.Unmarshal(expected, &expectedJSON)
+	if err != nil {
+		t.Errorf("error unmarshaling GoldenFile %s into RunSummary, Error: %s\n", expected, err)
+	}
+
+	if len(expectedJSON.EndpointRunSummary) != len(runSummary.EndpointRunSummary) {
+		t.Errorf("expected %d endpoints, got %d", len(expectedJSON.EndpointRunSummary), len(runSummary.EndpointRunSummary))
+	}
+	if len(expectedJSON.EndpointOverviewSummary) != len(runSummary.EndpointOverviewSummary) {
+		t.Errorf("expected %d endpoints, got %d", len(expectedJSON.EndpointOverviewSummary), len(runSummary.EndpointOverviewSummary))
+	}
+
+	if expectedJSON.RqstStats != runSummary.RqstStats {
+		t.Errorf("expected %+v, got %+v", expectedJSON.RqstStats, runSummary.RqstStats)
+	}
+	if expectedJSON.EndpointOverviewSummary[url1][http.MethodPut] != runSummary.EndpointOverviewSummary[url1][http.MethodPut] {
+		t.Errorf("expected %d PUTs for %s, got %d", expectedJSON.EndpointOverviewSummary[url1][http.MethodPut], url1,
+			runSummary.EndpointOverviewSummary[url1][http.MethodPut])
+	}
+	if expectedJSON.EndpointOverviewSummary[url1][http.MethodGet] != runSummary.EndpointOverviewSummary[url1][http.MethodGet] {
+		t.Errorf("expected %d GETs for %s, got %d", expectedJSON.EndpointOverviewSummary[url1][http.MethodGet], url1,
+			runSummary.EndpointOverviewSummary[url1][http.MethodPut])
+	}
+
+	if expectedJSON.EndpointOverviewSummary[url2][http.MethodPost] != runSummary.EndpointOverviewSummary[url2][http.MethodPost] {
+		t.Errorf("expected %d GETs for %s, got %d", expectedJSON.EndpointOverviewSummary[url2][http.MethodPost], url2,
+			runSummary.EndpointOverviewSummary[url2][http.MethodPost])
+	}
+
+	if expectedJSON.EndpointOverviewSummary[url3][http.MethodPost] != runSummary.EndpointOverviewSummary[url3][http.MethodPost] {
+		t.Errorf("expected %d GETs for %s, got %d", expectedJSON.EndpointOverviewSummary[url3][http.MethodPost], url3,
+			runSummary.EndpointOverviewSummary[url3][http.MethodPost])
+	}
+	if expectedJSON.EndpointOverviewSummary[url3][http.MethodPut] != runSummary.EndpointOverviewSummary[url3][http.MethodPut] {
+		t.Errorf("expected %d GETs for %s, got %d", expectedJSON.EndpointOverviewSummary[url3][http.MethodPut], url3,
+			runSummary.EndpointOverviewSummary[url3][http.MethodPut])
+	}
+	if expectedJSON.EndpointOverviewSummary[url3][http.MethodGet] != runSummary.EndpointOverviewSummary[url3][http.MethodGet] {
+		t.Errorf("expected %d GETs for %s, got %d", expectedJSON.EndpointOverviewSummary[url3][http.MethodGet], url3,
+			runSummary.EndpointOverviewSummary[url3][http.MethodGet])
+	}
+	if expectedJSON.EndpointOverviewSummary[url3][http.MethodDelete] != runSummary.EndpointOverviewSummary[url3][http.MethodDelete] {
+		t.Errorf("expected %d GETs for %s, got %d", expectedJSON.EndpointOverviewSummary[url3][http.MethodDelete], url3,
+			runSummary.EndpointOverviewSummary[url3][http.MethodDelete])
+	}
 }
 
 func updateGoldenFile(t *testing.T, testName string, contents string) {
@@ -84,11 +255,11 @@ func updateGoldenFile(t *testing.T, testName string, contents string) {
 	}
 }
 
-func readGoldenFile(t *testing.T, testName string) string {
+func readGoldenFile(t *testing.T, testName string) []byte {
 	gf := filepath.Join(goldenFileDir, testName+goldenFileSuffix)
 	gfc, err := ioutil.ReadFile(gf)
 	if err != nil {
 		t.Fatalf("failed reading golden file: %s", err)
 	}
-	return string(gfc)
+	return gfc
 }
